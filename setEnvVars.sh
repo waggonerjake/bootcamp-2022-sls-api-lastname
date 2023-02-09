a1498cd9702787640d570e31c547ef6745d2fa84
Jake Waggoner <jwaggoner@dminc.com>
2023-02-09 14:36:32 -0500

    Finish my app

--- setEnvVars.sh
+++ setEnvVars.sh
@@ -1,3 +1,3 @@
-export GITHUB_USERNAME="<<username>>"
-export GITHUB_REPO="bootcamp-2022-sls-api-<<lastname>>"
-export BUCKET_NAME="<<username>>-bootcamp-2022-bucket" #username must be all lowercase
+export GITHUB_USERNAME="waggonerjake"
+export GITHUB_REPO="bootcamp-2022-sls-api-lastname"
+export BUCKET_NAME="jwaggon-bootcamp-2022-bucket" #username must be all lowercase
